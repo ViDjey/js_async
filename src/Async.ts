7e6702612f1324aa6512263014e87fef218a4030
Viktoria <vikabu2002@mail.ru>
2022-04-09 16:16:55 +0300

    Асинхронность в JS

--- src/Async.ts
+++ src/Async.ts
@@ -1,8 +1,16 @@
+import { rejects } from 'assert';
+
 /*
 Создайте функцию mock, которая принимает на вход аргумент number (количество миллисекунд) и возвращает Promise,
 который завершится через заданное количество миллисекунд со значением, переданным в аргумент.
  */
 export function mock(ms: number): Promise<number> {
+    const pr = new Promise<number>((resolve) => {
+        setTimeout(() => {
+            resolve(ms);
+        }, ms);
+    });
+    return pr;
 }
 
 /*
@@ -10,7 +18,17 @@ export function mock(ms: number): Promise<number> {
  */
 export function getData(): Promise<number[]> {
     const result: number[] = [];
-
+    mock(100).then((data1) => {
+        result.push(data1);
+    });
+    mock(200).then((data2) => {
+        result.push(data2);
+    });
+    return mock(300).then((data3) => {
+        result.push(data3);
+        return result;
+    });
+    /*
     return mock(100)
         .then((data1) => {
             result.push(data1);
@@ -23,7 +41,7 @@ export function getData(): Promise<number[]> {
         .then((data3) => {
             result.push(data3);
             return result;
-        });
+        })*/
 }
 
 /*
@@ -32,8 +50,8 @@ export function getData(): Promise<number[]> {
  */
 export async function catchException(): Promise<string | undefined> {
     try {
-        Promise.reject(new Error('my error'));
+        await Promise.reject(new Error('my error'));
     } catch (err) {
-        return err.message;
+        return (err as Error).message;
     }
 }
